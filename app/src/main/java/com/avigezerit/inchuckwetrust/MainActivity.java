ae2e1cbfe4d8f949d9294a3b4e56326c0b16f359
Shaharli <avigezerit@gmail.com>
2016-07-29 15:28:39 +0300

    shareable using storage

--- app/src/main/java/com/avigezerit/inchuckwetrust/MainActivity.java
+++ app/src/main/java/com/avigezerit/inchuckwetrust/MainActivity.java
@@ -1,6 +1,10 @@
 package com.avigezerit.inchuckwetrust;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
@@ -8,6 +12,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -33,21 +39,26 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private static final String REQUEST_JOKE = "http://api.icndb.com/jokes/random/";
 
+    //permission
+    private static final int REQUEST_PERMISSION = 101;
+    private boolean permission_granted = false;
+    String[] myPermissions;
+
     TextView quoteTV;
     TextView creditTV;
     Button getRandBtn;
-    getRandomQuote getRandomQuote;
+    getRandomJoke getRandomJoke;
     String randJoke = null;
 
     View viewToSave;
+    View viewClickedShareMethod;
     Uri path;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,44 +66,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         quoteTV = (TextView) findViewById(R.id.quoteTV);
-        requestRandQuote();
+        requestRandJoke();
 
         creditTV = (TextView) findViewById(R.id.creditTV);
 
         getRandBtn = (Button) findViewById(R.id.getRandBtn);
-        getRandBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                quoteTV.setText("Chucking...");
-                requestRandQuote();
-            }
-        });
 
+        //xml ref
         ImageButton shareBtn = (ImageButton) findViewById(R.id.sBtnShare);
+        ImageButton sBtnWhap = (ImageButton) findViewById(R.id.sBtnWhap);
+        ImageButton sBtnFace = (ImageButton) findViewById(R.id.sBtnFace);
+
+        getRandBtn.setOnClickListener(this);
+        shareBtn.setOnClickListener(this);
+        sBtnFace.setOnClickListener(this);
+        sBtnWhap.setOnClickListener(this);
 
         viewToSave = findViewById(R.id.viewToSave);
-
-        shareBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                creditTV.setText("Sent by 'In Chuck We Trust' App!");
-                SaveImageToStorage(getBitmapFromView(viewToSave));
-
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_SEND);
-                intent.setType("image/*");
-
-                intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Chuck Joke");
-                intent.putExtra(Intent.EXTRA_STREAM, path);
-                startActivity(Intent.createChooser(intent, "Share Chuck Joke"));
-            }
-        });
     }
 
     public static Bitmap getBitmapFromView(View view) {
 
         //Define a bitmap with the same size as the view
-        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
         //Bind a canvas to it
         Canvas canvas = new Canvas(returnedBitmap);
         //Get the view's background
@@ -109,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         //create and get directory
         String root = Environment.getExternalStorageDirectory().getAbsolutePath();
         File myDir = new File(root + "/ChuckJoke/images");
-        if (!myDir.exists()){
+        if (!myDir.exists()) {
             if (myDir.mkdirs()) {
                 Log.e(TAG, "Directory created");
             }
@@ -117,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
 
         //set name of file
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        String fname = "Chuck_Joke_" +"_"+timeStamp+ ".jpg";
+        String fname = "Chuck_Joke_" + "_" + timeStamp + ".jpg";
 
         //new file based on name and directory
         File file = new File(myDir, fname);
@@ -137,20 +133,22 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        path =  Uri.parse("file://" + file.getAbsolutePath());
+        path = Uri.parse("file://" + file.getAbsolutePath());
 
     }
 
 
-    private void requestRandQuote() {
+    private void requestRandJoke() {
+
+        quoteTV.setText("Chucking...");
 
         String jokeSB = null;
 
-        getRandomQuote = new getRandomQuote();
-        getRandomQuote.execute(REQUEST_JOKE);
+        getRandomJoke = new getRandomJoke();
+        getRandomJoke.execute(REQUEST_JOKE);
 
         try {
-            jokeSB = getRandomQuote.get();
+            jokeSB = getRandomJoke.get();
         } catch (InterruptedException e) {
             Log.d(TAG, e.getMessage());
         } catch (ExecutionException e) {
@@ -171,7 +169,12 @@ public class MainActivity extends AppCompatActivity {
 
             JSONObject jokeValue = allData.getJSONObject("value");
 
+            //check for quotation marks and replace
             randJoke = jokeValue.getString("joke");
+
+            if (randJoke.contains("&quot;")) {
+                randJoke = randJoke.replace("&quot;", "\"");
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -180,8 +183,106 @@ public class MainActivity extends AppCompatActivity {
         quoteTV.setText(randJoke);
     }
 
+    @Override
+    public void onClick(View v) {
 
-    private class getRandomQuote extends AsyncTask<String, String, String> {
+        viewClickedShareMethod = v;
+
+        if (v.getId() == R.id.getRandBtn) {
+            requestRandJoke();
+        } else {
+            checkForPermissions();
+            if (permission_granted) {
+                shareJokeImage();
+            }
+        }
+    }
+
+    private void shareJokeImage() {
+
+        creditTV.setText("Created by 'In Chuck We Trust' App, Now on Google play!");
+        SaveImageToStorage(getBitmapFromView(viewToSave));
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+
+        switch (viewClickedShareMethod.getId()) {
+            case R.id.sBtnShare:
+                intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Chuck Joke");
+                intent.putExtra(Intent.EXTRA_TEXT, "Have you heard this one before?\nCheck out 'In Chuck We Trust' App!");
+                intent.putExtra(Intent.EXTRA_STREAM, path);
+                startActivity(Intent.createChooser(intent, "Share Chuck Joke"));
+                break;
+            case R.id.sBtnWhap:
+                intent.setPackage("com.whatsapp");
+                intent.putExtra(Intent.EXTRA_TEXT, "Have you heard this one before?\nCheck out 'In Chuck We Trust' App!");
+                intent.putExtra(Intent.EXTRA_STREAM, path);
+                startActivity(Intent.createChooser(intent, "Share Chuck Joke"));
+                break;
+            case R.id.sBtnFace:
+                intent.setPackage("com.facebook.katana");
+                intent.putExtra(Intent.EXTRA_STREAM, path);
+                startActivity(Intent.createChooser(intent, "Share Chuck Joke"));
+                break;
+        }
+
+        clearBgCredit();
+    }
+
+
+    private void checkForPermissions() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            //permission Dialog
+            myPermissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            requestPermissions(myPermissions, REQUEST_PERMISSION);
+
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                showMessageOKCancel("Please allow access to storage so you can share the Chuck Joke.",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                checkForPermissions();
+                            }
+                        });
+                return;
+            }
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            permission_granted = true;
+            shareJokeImage();
+
+        } else {
+            permission_granted = false;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private void clearBgCredit() {
+        creditTV.setText("");
+        viewToSave.setBackground(null);
+    }
+
+    private class getRandomJoke extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... params) {
@@ -234,5 +335,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
 
 }
